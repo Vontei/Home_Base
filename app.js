0164240d544014e492ec9c56cdccf3345301e17f
Nick Mattei <nmattei89@gmail.com>
2015-12-14 18:16:52 -0700

    tighten lines

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ $(function(){
   var currdeg = 0;
   $(".next").on("click", { d: "n" }, rotate);
   $(".prev").on("click", { d: "p" }, rotate);
+  $(".x").on("click", { d: "x" }, rotate);
+  $(".y").on("click", { d: "y" }, rotate);
+  $(".z").on("click", { d: "z" }, rotate);
 
   function rotate(e){
     if(e.data.d=="n"){
@@ -19,24 +22,35 @@ $(function(){
     });
   }
 
-$('.contact').hide();
-$('.thanks').click(function(){
-    $('.contact').show();
-    $('.thanks').hide();
-});
-$('.contact').click(function(){
-    $('.thanks').show();
     $('.contact').hide();
-});
-$('.job').hide();
-$('.name').click(function(){
-    $('.job').show();
-    $('.name').hide();
-});
-$('.job').click(function(){
-    $('.name').show();
+
+    $('.thanks').click(function(){
+        $('.contact').show();
+        $('.thanks').hide();
+    });
+
+    $('.contact').click(function(){
+        $('.thanks').show();
+        $('.contact').hide();
+    });
+
     $('.job').hide();
-});
+    $('.name').click(function(){
+        $('.job').show();
+        $('.name').hide();
+    });
+
+    $('.job').click(function(){
+        $('.name').show();
+        $('.job').hide();
+    });
+
+    // var item = $('.item')
+    //
+    // item.click(function () {
+    //    $('span').toggle();
+    // })
+
 
 
 })
